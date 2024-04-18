dfb78df50978864f7598482a1c4560104397b701
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-18 16:17:42 +0200

    add: carousel item

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-const images = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
-
 // Milestone 1:
 // Nel markup allegato rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 // Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva con i relativi titolo e testo diventerà visibile.
@@ -41,4 +13,46 @@ const images = [
 
 // BONUS 3:
 // Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+
+const { createApp } = Vue;
+
+createApp ({
+  data() {
+    return {
+      images: [
+        {
+          image: "img/01.webp",
+          title: "Marvel's Spiderman Miles Morale",
+          text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+        },
+        {
+          image: "img/02.webp",
+          title: "Ratchet & Clank: Rift Apart",
+          text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        },
+        {
+          image: "img/03.webp",
+          title: "Fortnite",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        },
+        {
+          image: "img/04.webp",
+          title: "Stray",
+          text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        },
+        {
+          image: "img/05.webp",
+          title: "Marvel's Avengers",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        },
+      ],
+      index: 0,
+    }
+  },
+  methods: {
+    showImage: function(curElem) {
+      curEle
+    }
+  }
+}).mount("#app")
 
